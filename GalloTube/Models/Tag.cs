616e4b80c53d6c817b6e34f9e966106d45b0dc2e
Mariah <liviamariahzaratini@gmail.com>
2023-08-15 12:27:54 -0300

    alteração do flix para tube

---
+++ GalloTube/Models/Tag.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace GalloTube.Models;
+
+[Table("Tag")]
+public class Tag
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public byte Id { get; set; }
+    
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "O nome do Gênero é obrigatório")]
+    [StringLength(30, ErrorMessage = "O Nome deve possuir no máximo 30 caracteres")]
+    public string Name { get; set; }    
+    public ICollection<VideoTag> Video { get; set; }
+}
